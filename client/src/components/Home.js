c5ba414ac40cb36d69b12661142312edebecafa0
matts112 <mttswinford@yahoo.com>
2019-01-19 10:53:48 -0700

    update home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 export default class Home extends Component {
-    render() {
+    handleClick = () => {
+        console.log('this is:', this);
+      }
+    
+      render() {
         return (
-            <div>
-                Home Component
-            </div>
+          <button onClick={this.handleClick}>
+            <Link to='/ActorSearch'>ActorSearch</Link>
+          </button>
         );
+      }
     }
-}
